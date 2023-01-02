a2825e498e069b459abfe910f63876dc562835d1
Kate Volkova <let.kate.test@gmail.com>
2023-01-02 16:52:34 +0100

    Final version

--- src/test/java/unbiased_1/FindAdviserTest.java
+++ src/test/java/unbiased_1/FindAdviserTest.java
@@ -2,13 +2,15 @@ package unbiased_1;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.selector.ByText;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class FindAdviser extends BaseTest {
+public class FindAdviserTest extends BaseTest {
     private final static String baseURL = "https://www.unbiased.co.uk/";
     private final ElementsCollection itemsCollection = $$x("//div[@tabindex=0]");
     private final static String adviseItem = "Financial planning";
@@ -16,7 +18,7 @@ public class FindAdviser extends BaseTest {
     private final SelenideElement postcode = $x("//input[@id=\"filter-select-postcode\"]");
     private final static String postcodeValue = "SE207AA";
     private final SelenideElement submitButton = $x("//button[@id=\"filter-submit-button\"]");
-    private final SelenideElement visibleElement = $("#directory-search-results > div.container.py-0.px-sm-5.px-lg-8.px-xl-12.container--fluid > div.text-center.py-3.show-all-advisors-filter > div > div > div > label");
+    private final SelenideElement visibleElement = $(byText("Show only advisers available for contact"));
     private final ElementsCollection resultsList = $$x("//h2");
     private final String adviserName = "The Private Office";
 
